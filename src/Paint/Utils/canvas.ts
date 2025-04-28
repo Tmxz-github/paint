5f5ff3d67a07cc98a2d5402322a490267fc9e97c
Tmxz-github <645284050@qq.com>
2025-04-28 16:31:14 +0800

    feat: createCanvasContext 新增函数重构

--- src/Paint/Utils/canvas.ts
+++ src/Paint/Utils/canvas.ts
@@ -1,27 +1,33 @@
-export const createCanvasContext = (
-	width: number,
-	height: number
-): OffscreenCanvasRenderingContext2D | CanvasRenderingContext2D => {
-	if (OffscreenCanvas) {
-		const c = new OffscreenCanvas(width, height);
-		const ctx = c.getContext("2d");
-		if (!ctx) {
-			return createCanvasContext2(width, height);
-		} else {
-			return ctx;
-		}
-	} else {
-		return createCanvasContext2(width, height);
-	}
-};
+type Canvas = OffscreenCanvas | HTMLCanvasElement;
+type context2D = OffscreenCanvasRenderingContext2D | CanvasRenderingContext2D;
 
-const createCanvasContext2 = (width: number, height: number): CanvasRenderingContext2D => {
-	const c = document.createElement("canvas");
-	c.width = width;
-	c.height = height;
-	const ctx = c.getContext("2d");
+export function createCanvasContext(imagedata: ImageData): context2D;
+export function createCanvasContext(width: number, height: number): context2D;
+export function createCanvasContext(widthOrImageData: number | ImageData, height?: number): context2D {
+	let canvas: Canvas;
+	let ctx: context2D | null;
+	let width = 0;
+	if (widthOrImageData instanceof ImageData) {
+		width = widthOrImageData.width;
+		height = widthOrImageData.height;
+	} else if (typeof widthOrImageData === "number" && typeof height === "number") {
+		width = widthOrImageData;
+	} else {
+		throw new Error("wrong args");
+	}
+
+	canvas =
+		typeof OffscreenCanvas !== "undefined" ? new OffscreenCanvas(width, height) : document.createElement("canvas");
+
+	canvas.width = width;
+	canvas.height = height;
+	ctx = canvas.getContext("2d");
+
 	if (!ctx) {
 		throw new Error("cant get ctx");
 	}
+	if (widthOrImageData instanceof ImageData) {
+		ctx.putImageData(widthOrImageData, 0, 0);
+	}
 	return ctx;
-};
+}
